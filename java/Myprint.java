63f9471be2628b612152a19e34c61bb6be8c9789
marigichun <marigichun@gmail.com>
2017-12-12 12:37:40 +0900

    Myprint 연습3일 헤멤

---
+++ java/Myprint.java
@@ -0,0 +1,8 @@
+public class Myprint {
+	// main: mathod(메서드),프로그램의 시작점
+	// string[] args: argument, parameter
+	public static void main(String[] args) {
+		// System.out.println 메서드:괄호 안의 글자를 출력
+		System.out.println("안녕 세상아");
+	}
+}
